fbc9db0163a7c48c05710be0c70b8b9df0ba4a54
“oscar.martinez.rubi” <omrubi@gmail.com>
2015-07-07 13:00:13 +0200

    put in function

--- python/merge_potree_all.py
+++ python/merge_potree_all.py
@@ -2,10 +2,6 @@
 import os, sys, time
 import merge_potree, utils
 
-inputFolder = sys.argv[1]
-outputFolder = sys.argv[2]
-tiles = os.listdir(inputFolder)
-
 def argument_parser():
     """ Define the arguments and return the parser object"""
     parser = argparse.ArgumentParser(
@@ -15,43 +11,47 @@ def argument_parser():
     return parser
 
 
-validTiles = []
-for tile in tiles:
-    dataAbsPath = inputFolder + '/' + tile + '/data'
-    if os.path.isdir(dataAbsPath) and len(os.listdir(dataAbsPath)):
-        validTiles.append(tile)
-    else:
-        print 'Ignoring ' + tile
-
-print ','.join(validTiles)
-
-stdout = sys.stdout
-stderr = sys.stderr
-
-for mIndex in range(1, len(validTiles)):
-    if mIndex == 1:
-        tileAInputFolder = inputFolder + '/' + validTiles[0]
-    else:
-        tileAInputFolder = outputFolder + '/' + 'tile_merged_%d' % (mIndex-1)
-    tileBInputFolder = inputFolder + '/' + validTiles[mIndex]
+def run(inputFolder, outputFolder):
+        
+    tiles = os.listdir(inputFolder)
     
-    tileOOutputFolder = outputFolder + '/' + 'tile_merged_%d' % mIndex
+    validTiles = []
+    for tile in tiles:
+        dataAbsPath = inputFolder + '/' + tile + '/data'
+        if os.path.isdir(dataAbsPath) and len(os.listdir(dataAbsPath)):
+            validTiles.append(tile)
+        else:
+            print 'Ignoring ' + tile
     
-    if os.path.isdir(tileOOutputFolder):
-        raise Exception(tileOOutputFolder + ' already exists!')
+    print ','.join(validTiles)
     
-    os.system('mkdir -p ' + tileOOutputFolder)
-    sys.stdout = open(tileOOutputFolder + '.log', 'w')
-    sys.stderr = open(tileOOutputFolder + '.err', 'w')
-    print 'Input Potree Octtree A: ', tileAInputFolder
-    print 'Input Potree Octtree B: ', tileBInputFolder
-    print 'Output Potree Octtree: ', tileOOutputFolder
-    merge_potree.run(tileAInputFolder, tileBInputFolder, tileOOutputFolder, True)
-    sys.stdout = stdout
-    sys.stderr = std.err
-    #command = 'python /home/oscar/sw/Massive-PotreeConverter/python/merge_potree.py -a ' + tileAInputFolder + ' -b ' + tileBInputFolder + ' -o ' + tileOOutputFolder + ' -m  &> ' + tileOOutputFolder + '.log'
-    #print command
-    #os.system(command)
+    stdout = sys.stdout
+    stderr = sys.stderr
+    
+    for mIndex in range(1, len(validTiles)):
+        if mIndex == 1:
+            tileAInputFolder = inputFolder + '/' + validTiles[0]
+        else:
+            tileAInputFolder = outputFolder + '/' + 'tile_merged_%d' % (mIndex-1)
+        tileBInputFolder = inputFolder + '/' + validTiles[mIndex]
+        
+        tileOOutputFolder = outputFolder + '/' + 'tile_merged_%d' % mIndex
+        
+        if os.path.isdir(tileOOutputFolder):
+            raise Exception(tileOOutputFolder + ' already exists!')
+        
+        os.system('mkdir -p ' + tileOOutputFolder)
+        sys.stdout = open(tileOOutputFolder + '.log', 'w')
+        sys.stderr = open(tileOOutputFolder + '.err', 'w')
+        print 'Input Potree Octtree A: ', tileAInputFolder
+        print 'Input Potree Octtree B: ', tileBInputFolder
+        print 'Output Potree Octtree: ', tileOOutputFolder
+        merge_potree.run(tileAInputFolder, tileBInputFolder, tileOOutputFolder, True)
+        sys.stdout = stdout
+        sys.stderr = std.err
+        #command = 'python /home/oscar/sw/Massive-PotreeConverter/python/merge_potree.py -a ' + tileAInputFolder + ' -b ' + tileBInputFolder + ' -o ' + tileOOutputFolder + ' -m  &> ' + tileOOutputFolder + '.log'
+        #print command
+        #os.system(command)
     
 if __name__ == "__main__":
     args = argument_parser().parse_args()
